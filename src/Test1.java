66120ff07b8d766950a6a5d3aab5b3b4adfd41ea
YangIoy <121197143@qq.com>
2023-01-13 00:04:07 +0800

    回车的快捷键

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,6 @@
 public class Test1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) { //main -> public static void main(String[] args){}
+        System.out.println("Hello World!"); //sout -> System.out.println();
+        System.out.println(10); //10.sout -> System.out.println(10);
     }
 }
