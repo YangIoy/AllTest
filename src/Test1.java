329590f76a1f84d197916ad0c3e22269fc20a40a
YangIoy <121197143@qq.com>
2023-01-13 00:12:26 +0800

    了解了main、打印、for循环的快捷键

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,9 @@
 public class Test1 {
-    public static void main(String[] args) { //main -> public static void main(String[] args){}
+    public static void main(String[] args) { //main or psvm -> public static void main(String[] args){}
         System.out.println("Hello World!"); //sout -> System.out.println();
         System.out.println(10); //10.sout -> System.out.println(10);
+        for (int i = 0; i < 10; i++) { //fori -> for (int i = 0; i < ; i++)
+            System.out.println("循环");
+        }
     }
 }
