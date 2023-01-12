9daac7f98658b20afc583d33a31cd2971611ab22
YangIoy <121197143@qq.com>
2023-01-13 01:01:46 +0800

    行注释 段注释 文档注释

--- src/Test1.java
+++ src/Test1.java
@@ -1,4 +1,6 @@
 public class Test1 {
+    //段注释
+    /*
     public static void main(String[] args) { //main or psvm -> public static void main(String[] args){}
         System.out.println("Hello World!"); //sout -> System.out.println();
         System.out.println(10); //10.sout -> System.out.println(10);
@@ -6,4 +8,17 @@ public class Test1 {
             System.out.println("循环");
         }
     }
+    */
+
+    //文档注释
+
+    /**
+     * 本方法还不知道要干什么
+     * @param args 无
+     */
+    public static void main(String[] args) {
+
+    }
+
+
 }
